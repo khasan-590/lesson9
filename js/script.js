0099781d5ca1bd18a940fd442c02bece2bc84823
khasan-590 <khasan4ik590@gmail.com>
2021-02-13 20:41:48 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-let getCalculate = document.getElementById('#start');
+let getCalculate = document.getElementById('start');
 // console.log(document.getElementById('start'))
 
 let getPlus = (document.getElementsByTagName('button')[0] , [1]);
@@ -18,12 +18,15 @@ let getBudgetMonth = document.getElementsByClassName('budget_month-value'),
 		getExpensesMonth = document.getElementsByClassName('expenses_month-value'),
 		getAddIncome = document.getElementsByClassName('additional_income-value'),
 		getAddExpenses = document.getElementsByClassName('additional_expenses-value'),
+		getAddName = document.getElementsByClassName('additional_expenses-item'),
 		getIncomePeriod = document.getElementsByClassName('income_period-value'),
-		getTargetMonth = document.getElementsByClassName('target_month-value');
+		getTargetMonth = document.getElementsByClassName('target_month-value'),
+		getExpenseName = document.getElementsByClassName('expenses-title'),
+		getExpenseSum = document.getElementsByClassName('expenses-amount'),
+		getTargetAmount = document.getElementsByClassName('target-amount');
 
 // console.log(document.getElementsByClassName('budget_day-value'));
 
 let getSum = document.querySelector('.deposit-amount'),
 		getProcent = document.querySelector('.deposit-percent'),
 		getPeriodSelect = document.querySelector('.period-select');
-		
